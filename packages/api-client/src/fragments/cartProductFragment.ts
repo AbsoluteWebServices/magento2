a6c6106400295619490504d3015b2078a1db277b
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2021-08-09 17:31:04 -0400

    feat(cart): Query free gift info on cart items

--- packages/api-client/src/fragments/cartProductFragment.ts
+++ packages/api-client/src/fragments/cartProductFragment.ts
@@ -1,13 +1,18 @@
 import gql from 'graphql-tag';
 import ProductData from './productFragment';
+import FreeGiftData from './freeGiftFragment';
 
 export default gql`
   ${ProductData}
-  
+  ${FreeGiftData}
+
   fragment CartProductData on CartItemInterface {
   uid
   product {
     ...ProductData
+  }
+  mp_free_gifts {
+    ...FreeGiftData
   }
   prices {
     row_total {
