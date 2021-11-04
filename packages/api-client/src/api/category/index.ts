06120d87eb8c0853b54eab1eb0566aa0bb568989
Dmitry Kozachek <dmitry-kozachek@users.noreply.github.com>
2021-11-04 12:56:27 -0400

    feat(preview): Add staging preview

--- packages/api-client/src/api/category/index.ts
+++ packages/api-client/src/api/category/index.ts
@@ -1,14 +1,23 @@
 import { ApolloQueryResult } from '@apollo/client';
-import { CategoryQueryFocus, CategorySearchQueryVariables } from '../../types/GraphQL';
+import { CategoryQueryFocus, CategorySearchQueryVariables, StagingPreviewParams } from '../../types/GraphQL';
 import category from './category';
 import { Context } from '../../types/context';
 
 export default async (
   { client }: Context,
   params: CategorySearchQueryVariables,
+  preview?: StagingPreviewParams,
 ): Promise<ApolloQueryResult<CategoryQueryFocus>> => client
   .query<CategoryQueryFocus, CategorySearchQueryVariables>({
   query: category,
   variables: { ...params },
   fetchPolicy: 'cache-first',
+  ...(preview ? {
+    context: {
+      headers: {
+        Authorization: `Bearer ${preview.accessToken}`,
+        'Preview-Version': preview.version,
+      },
+    },
+  } : {}),
 });
