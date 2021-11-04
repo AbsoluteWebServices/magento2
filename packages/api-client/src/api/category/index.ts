cb7df7cb2e68bb544bbb95a41b1557c29524edde
Dmitry Kozachek <dmitry-kozachek@users.noreply.github.com>
2021-11-04 12:56:27 -0400

    feat(preview): Add staging preview

--- packages/api-client/src/api/category/index.ts
+++ packages/api-client/src/api/category/index.ts
@@ -1,18 +1,31 @@
 import { ApolloQueryResult } from '@apollo/client/core';
-import { CategoryQuery, CategorySearchQueryVariables, CachedQuery } from '../../types/GraphQL';
+import {
+  CategoryQuery, CategorySearchQueryVariables, CachedQuery, StagingPreviewQueryVariables,
+} from '../../types/GraphQL';
 import category from './category';
 import { Context } from '../../types/context';
 
 export default async (
   { client }: Context,
-  filters: CategorySearchQueryVariables,
-):  Promise<ApolloQueryResult<CachedQuery<CategoryQuery>>> => {
+  {
+    filters,
+    preview,
+  }: StagingPreviewQueryVariables<CategorySearchQueryVariables>,
+): Promise<ApolloQueryResult<CachedQuery<CategoryQuery>>> => {
   const result = await client
-  .query<CachedQuery<CategoryQuery>, CategorySearchQueryVariables>({
-  query: category,
-  variables: { ...filters },
-  fetchPolicy: 'cache-first',
-});
+    .query<CachedQuery<CategoryQuery>, CategorySearchQueryVariables>({
+    query: category,
+    variables: { filters },
+    fetchPolicy: 'cache-first',
+    ...(preview ? {
+      context: {
+        headers: {
+          Authorization: `Bearer ${preview.accessToken}`,
+          'Preview-Version': preview.version,
+        },
+      },
+    } : {}),
+  });
 
   if (!result.data.categoryList?.length) {
     result.data.cacheTags = 'empty';
--- packages/api-client/src/types/GraphQL.ts
+++ packages/api-client/src/types/GraphQL.ts
@@ -7942,3 +7942,12 @@ export type FocusCartItemStockError = {
   error_code: string;
   message: string;
 };
+
+export interface StagingPreviewParams {
+  version: string;
+  accessToken: string;
+};
+
+export type StagingPreviewQueryVariables<QUERY> = QUERY & {
+  preview?: StagingPreviewParams;
+};
