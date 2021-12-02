795a22d4945fbca2c36d68beeca1056e298f0b51
Vladimir Lisniak <vladimir@absoluteweb.com>
2021-12-02 16:48:39 +0200

    feat(FOCUS-478): Update guest request return mutation

--- packages/api-client/src/api/focusGuestRequestReturn/focusGuestRequestReturn.ts
+++ packages/api-client/src/api/focusGuestRequestReturn/focusGuestRequestReturn.ts
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 import CustomerReturnFragment from '../../fragments/customerReturnFragment';
 
 export default gql`
-  mutation focusGuestRequestReturn($input: FocusGuestRequestReturnInput) {
+  mutation focusGuestRequestReturn($input: FocusGuestRequestReturnInput!) {
     focusGuestRequestReturn(input: $input) {
       return {
         ${CustomerReturnFragment}
