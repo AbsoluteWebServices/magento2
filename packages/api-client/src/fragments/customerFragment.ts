7fdb6701ecd5dfd03f0a0cf22a4311db9b735c0a
Vladimir Lisniak <vladimir@absoluteweb.com>
2021-12-17 21:29:03 +0200

    feat(FOCUS-671): Add idme data to customer query
    feat(FOCUS-671): Move idme_data outside of addresses field (#62)
    
    * feat(FOCUS-671): Move idme_data outside of addresses field
    
    * feat(FOCUS-671): Update version
    
    Co-authored-by: Vladimir Lisniak <vladimir@absoluteweb.com>

--- packages/api-client/src/fragments/customerFragment.ts
+++ packages/api-client/src/fragments/customerFragment.ts
@@ -34,4 +34,20 @@ addresses {
   suffix
   telephone
 }
+idme_data {
+  uuid
+  first_name
+  last_name
+  email
+  zip
+  is_verified
+  group
+  subgroup
+  user_errors
+  group_data {
+    handle
+    name
+    value
+  }
+}
 `;
