e8325ef0a68a8b1f548c82024870c4bd4621c865
Dmitry Kozachek <dmitry-kozachek@users.noreply.github.com>
2022-01-04 11:47:52 -0500

    feat(products): Remove aggregations query from products list

--- packages/api-client/src/api/products/productsList.ts
+++ packages/api-client/src/api/products/productsList.ts
@@ -4,15 +4,6 @@ import productFragment from '../../fragments/productFragment';
 export default gql`
   query productsList($search: String = "", $filter: ProductAttributeFilterInput, $pageSize: Int = 10, $currentPage: Int = 1, $sort: ProductAttributeSortInput) {
     products(search: $search, filter: $filter, pageSize: $pageSize, currentPage: $currentPage, sort: $sort) {
-      aggregations {
-        attribute_code
-        label
-        options {
-          label
-          value
-          count
-        }
-      }
       items {
         ${productFragment}
       }
