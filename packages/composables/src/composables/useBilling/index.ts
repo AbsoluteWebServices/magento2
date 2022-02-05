a1a1d2129f2e3358a9118cb86046e7cbb5555ab0
Dmitry Kozachek <dmitry-kozachek@users.noreply.github.com>
2022-02-05 00:20:48 -0500

    feat(checkout): Improve checkout composables

--- packages/composables/src/composables/useBilling/index.ts
+++ packages/composables/src/composables/useBilling/index.ts
@@ -5,12 +5,14 @@ import {
   UseBillingParams,
 } from '@absolute-web/vsf-core';
 import {
+  BillingCartAddress,
+  BillingAddressInput,
   SetBillingAddressOnCartInput,
 } from '@absolute-web/magento-api';
 import useCart from '../useCart';
 import useShippingProvider from '../useShippingProvider';
 
-const factoryParams: UseBillingParams<any, any> = {
+const factoryParams: UseBillingParams<BillingCartAddress, BillingAddressInput> = {
   provide() {
     return {
       useShippingProvider: useShippingProvider(),
@@ -29,42 +31,13 @@ const factoryParams: UseBillingParams<any, any> = {
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  save: async (context: Context, params) => {
+  save: async (context: Context, { params }) => {
     Logger.debug('[Magento] setBillingAddress');
     const { id } = context.cart.cart.value;
 
-    const {
-      apartment,
-      neighborhood,
-      extra,
-      sameAsShipping,
-      customerAddressId,
-      ...address
-    } = params.billingDetails;
-
-    const street = [address.street];
-
-    if (apartment) street.push(apartment);
-
-    if (neighborhood) street.push(neighborhood);
-
-    if (extra) street.push(extra);
-
-    const billingData = customerAddressId
-      ? ({
-        customer_address_id: customerAddressId,
-      })
-      : ({
-        address: {
-          ...address,
-          street,
-        },
-        same_as_shipping: sameAsShipping,
-      });
-
     const setBillingAddressOnCartInput: SetBillingAddressOnCartInput = {
       cart_id: id,
-      billing_address: billingData,
+      billing_address: params,
     };
 
     const { data } = await context.$magento.api.setBillingAddressOnCart(
@@ -104,4 +77,4 @@ const factoryParams: UseBillingParams<any, any> = {
   },
 };
 
-export default useBillingFactory<any, any>(factoryParams);
+export default useBillingFactory<BillingCartAddress, BillingAddressInput>(factoryParams);
--- packages/composables/src/composables/usePaymentProvider/index.ts
+++ packages/composables/src/composables/usePaymentProvider/index.ts
@@ -4,12 +4,13 @@ import {
 } from '@absolute-web/vsf-core';
 import {
   PaymentMethodInput,
+  SelectedPaymentMethod,
   SetPaymentMethodOnCartInputs,
 } from '@absolute-web/magento-api';
 import useCart from '../useCart';
 import { usePaymentProviderFactory, UsePaymentProviderParams } from '../../factories/usePaymentProviderFactory';
 
-const factoryParams: UsePaymentProviderParams<any, PaymentMethodInput> = {
+const factoryParams: UsePaymentProviderParams<SelectedPaymentMethod, PaymentMethodInput> = {
   provide() {
     return {
       cart: useCart(),
@@ -17,17 +18,15 @@ const factoryParams: UsePaymentProviderParams<any, PaymentMethodInput> = {
   },
   load: async (context: Context, { customQuery }) => {
     Logger.debug('[Magento] loadPaymentProvider', { customQuery });
-    const cartId = context.cart.cart.value.id;
-    const { data } = await context
-      .$magento
-      .api
-      .getAvailablePaymentMethods({ cartId });
 
-    Logger.debug('[Result]:', { data });
+    if (!context.cart.cart?.value?.selected_payment_method) {
+      await context.cart.load({ customQuery });
+    }
 
-    return data
+    return context
       .cart
-      .available_payment_methods;
+      .cart
+      .value?.selected_payment_method;
   },
 
   save: async (context: Context, params) => {
@@ -47,11 +46,16 @@ const factoryParams: UsePaymentProviderParams<any, PaymentMethodInput> = {
 
     Logger.debug('[Result]:', { data });
 
-    return data
-      .setPaymentMethodOnCart
-      .cart
-      .available_payment_methods;
+    const { cart } = data
+      .setPaymentMethodOnCart;
+
+    context.cart.setCart({
+      ...context.cart.cart.value,
+      selected_payment_method: cart.selected_payment_method,
+    });
+
+    return cart.selected_payment_method;
   },
 };
 
-export default usePaymentProviderFactory<any, PaymentMethodInput>(factoryParams);
+export default usePaymentProviderFactory<SelectedPaymentMethod, PaymentMethodInput>(factoryParams);
