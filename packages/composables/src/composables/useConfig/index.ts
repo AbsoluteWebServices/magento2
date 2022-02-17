2c3713ce0ca71d9d08d15a4716bb2c4ca23db8bd
Dmitry Kozachek <dmitry-kozachek@users.noreply.github.com>
2022-02-17 15:25:49 -0500

    feat(cache): Add cache context

--- packages/composables/src/composables/useConfig/index.ts
+++ packages/composables/src/composables/useConfig/index.ts
@@ -1,11 +1,20 @@
 import { Context } from '@absolute-web/vsf-core';
 import { StoreConfig } from '@absolute-web/magento-api';
+import useUser from '../useUser';
 import { useConfigFactory, UseConfigFactoryParams } from '../../factories/useConfigFactory';
 import { UseConfig } from '../../types/composables';
 
 const factoryParams: UseConfigFactoryParams<StoreConfig> = {
+  provide() {
+    return {
+      user: useUser(),
+    };
+  },
   loadConfig: async (context: Context, params) => {
     const { data } = await context.$magento.getApi.storeConfig();
+
+    context.$magento.config.state.setContext(data.cacheId || null);
+    context.user.updateContext();
 
     return data.storeConfig || {};
   },
